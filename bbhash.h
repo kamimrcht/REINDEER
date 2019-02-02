6f05176ce9cf3e7a69f3eef730a1eb20050d64ec
Piezoid <mael.kerbiriou@free.fr>
2019-02-02 20:07:16 +0000

    refactoring bbhash
    Removing the bit offset field as it can be recomputed easily while
    traversing levels.

--- bbhash.h
+++ bbhash.h
@@ -163,16 +163,16 @@ we need this 2-functors scheme because HashFunctors won't work with unordered_ma
 		uint64_t operator ()  (const Item& key, uint64_t seed=0xAAAAAAAA55555555ULL) const  {  return hash64(key, seed);  }
 
 	private:
-		inline static uint64_t hash64 (uint64_t key, uint64_t seed) {
+		static uint64_t hash64 (uint64_t key, uint64_t seed) {
 			return hash_bis(key, seed);
 		}
 
-		inline static uint64_t hash64 (unsigned __int128& key, uint64_t seed)
+		static uint64_t hash64 (unsigned __int128& key, uint64_t seed)
 		{
 			return hash_bis ((uint64_t) (key >> 64), seed^0xAAAAAAAA55555555ULL)  ^  hash_bis ((uint64_t)key, seed^0xBBBBBBBB66666666ULL);
 		}
 
-		inline static uint64_t hash_bis (uint64_t key, uint64_t seed)
+		static uint64_t hash_bis (uint64_t key, uint64_t seed)
 		{
 			uint64_t hash = seed;
 			hash ^= (hash <<  7) ^  key * (hash >> 3) ^ (~((hash << 11) + (key ^ (hash >> 5))));
@@ -222,13 +222,13 @@ we need this 2-functors scheme because HashFunctors won't work with unordered_ma
 		public:
 
 
-		uint64_t h0(hash_pair_t  & s, const Item& key )
+		uint64_t h0(hash_pair_t  & s, const Item& key ) const
 		{
 			s[0] =  singleHasher (key, 0xAAAAAAAA55555555ULL);
 			return s[0];
 		}
 
-		uint64_t h1(hash_pair_t  & s, const Item& key )
+		uint64_t h1(hash_pair_t  & s, const Item& key ) const
 		{
 			s[1] =  singleHasher (key, 0x33333333CCCCCCCCULL);
 			return s[1];
@@ -236,7 +236,7 @@ we need this 2-functors scheme because HashFunctors won't work with unordered_ma
 
 
 		//return next hash an update state s
-		uint64_t next(hash_pair_t & s ) {
+		uint64_t next(hash_pair_t & s ) const {
 			uint64_t s1 = s[ 0 ];
 			const uint64_t s0 = s[ 1 ];
 			s[ 0 ] = s0;
@@ -244,7 +244,7 @@ we need this 2-functors scheme because HashFunctors won't work with unordered_ma
 			return ( s[ 1 ] = ( s1 ^ s0 ^ ( s1 >> 17 ) ^ ( s0 >> 26 ) ) ) + s0; // b, c
 		}
 
-		uint64_t hot_fun iter(const Item& key, hash_pair_t& s, size_t i) {
+		uint64_t hot_fun iter(const Item& key, hash_pair_t& s, size_t i) const {
 			uint64_t h;
 			if(i == 0)
 				h = h0(s, key);
@@ -378,8 +378,8 @@ we need this 2-functors scheme because HashFunctors won't work with unordered_ma
 		//clear collisions in interval, only works with start and size multiple of 64
 		void clearCollisions(uint64_t start, size_t size, const bitVector& cc)
 		{
-			assume( (start & 63) ==0, "start must be a multple of 64");
-			assume( (size & 63) ==0, "siaz must be a multple of 64");
+			assume( (start & 63) ==0, "start must be a multiple of 64");
+			assume( (size & 63) ==0, "size must be a multiple of 64");
 			uint64_t ids = (start/64ULL);
 			assume( ids + (size/64ULL) <= _nwords, "clearCollisions called after start");
 			for(uint64_t ii =0;  ii< (size/64ULL); ii++ )
@@ -392,11 +392,12 @@ we need this 2-functors scheme because HashFunctors won't work with unordered_ma
 		//clear interval, only works with start and size multiple of 64
 		void clear(uint64_t start, size_t size)
 		{
-			assume( (start & 63) ==0, "start must be a multple of 64");
-			assume( (size & 63) ==0, "siaz must be a multple of 64");
+			assume( (start & 63) ==0, "start must be a multiple of 64");
+			assume( (size & 63) ==0, "size must be a multiple of 64");
 			memset(_bitArray + (start/64ULL),0,(size/64ULL)*sizeof(uint64_t));
 		}
 
+#ifndef NDEBUG
 		//for debug purposes
 		void print() const
 		{
@@ -417,6 +418,7 @@ we need this 2-functors scheme because HashFunctors won't work with unordered_ma
 			}
 			printf("\n");
 		}
+#endif
 
 		//return value at pos
 		uint64_t operator[](uint64_t pos) const
@@ -539,51 +541,22 @@ we need this 2-functors scheme because HashFunctors won't work with unordered_ma
 	/* Hasher_t returns a single hash when operator()(elem_t key) is called.
 	   if used with XorshiftHashFunctors, it must have the following operator: operator()(elem_t key, uint64_t seed) */
 	template <typename elem_t, typename Hasher_t, unsigned _nb_levels=16>
-	class mphf {
+	class mphf : private XorshiftHashFunctors<elem_t,Hasher_t> { //Empty base opt
 		/* this mechanisms gets P hashes out of Hasher_t */
 		typedef XorshiftHashFunctors<elem_t,Hasher_t> MultiHasher_t ;
 
 		struct buildState {
 			bitVector accommodated;
 			bitVector collisions_map;
+			uint64_t level_offset;
 		};
-
-
-	struct levelIndexer {
-		levelIndexer(){ }
-
-		~levelIndexer() {
-		}
-
-		static uint64_t fastrange64(uint64_t word, uint64_t p) {
-			//return word %  p;
-
-			return (uint64_t)(((__uint128_t)word * (__uint128_t)p) >> 64);
-		}
-
-		uint64_t hash2levelidx(uint64_t hash_raw) const {
-			return fastrange64( hash_raw, hash_domain);
-		}
-
-
-		uint64_t hash2idx(uint64_t hash_raw) const {
-			return hash2levelidx(hash_raw) + idx_begin;
-		}
-
-		uint64_t endidx() const {
-			return idx_begin + hash_domain;
-		}
-
-		uint64_t idx_begin;
-		uint64_t hash_domain;
-	};
 
 	public:
 		mphf()
 		{}
 
 		template <typename Range>
-		mphf( size_t n, Range const& input_range, double gamma = 2.0) :
+		noinline_fun flatten_fun mphf( size_t n, Range const& input_range, double gamma = 2.0) :
 		_gamma(gamma), _nelem(n)
 		{
 			if(n == 0) return;
@@ -592,56 +565,52 @@ we need this 2-functors scheme because HashFunctors won't work with unordered_ma
 
 			buildState state = {
 				.accommodated = bitVector(_nelem),
-				.collisions_map = bitVector(_levels[0].hash_domain),
+				.collisions_map = bitVector(_hash_domains[0]),
+				.level_offset = 0
 			};
 
-			unsigned level = 0;
-			for(; ; level++) {
+			for(unsigned level = 0; ; level++) {
 				if(level < _nb_levels) {
 					if(processLevel(input_range, level, state)) {
 						continue; // Some keys collided, needs another level
 					} else {
+						bitset.build_ranks(state.level_offset);
 						break;
 					}
 				} else {
-					processFallbackLevel(input_range, state);
-					level = _nb_levels - 1;
+					uint64_t next_rank = bitset.build_ranks(bitset.size());
+					processFallbackLevel(input_range, state, next_rank);
 					break;
 				}
 			}
-			_lastbitsetrank = bitset.build_ranks(_levels[level].endidx());
 		}
 
-		uint64_t lookup(elem_t elem) const
+		hot_fun uint64_t lookup(elem_t elem) const
 		{
-			hash_pair_t bbhash;  unsigned level;
-			uint64_t bit_idx = getLevel(elem, level, bbhash);
-
-			if(level < _nb_levels) {
-				return bitset.rank(bit_idx);
+			const_level_finder finder(*this, elem);
+			if(finder) {
+				return finder.rank();
 			} else {
 				auto in_final_map = _final_hash.find(elem);
 				if (in_final_map != _final_hash.end())
 				{
-					return in_final_map->second + _lastbitsetrank;
+					return in_final_map->second;
 				} else {
 					// elem was not in orignal set of keys
 					assert(false, "not in final map");
 					return ULLONG_MAX; // means elem not in set
 				}
 			}
-			return bitset.rank(bit_idx);
 		}
 
-		uint64_t nbKeys() const
-		{
-			return _nelem;
+		uint64_t nbKeys() const { return _nelem; }
+		uint64_t bitvectorBitSize() const { return bitset.bitSize(); }
+		uint64_t hashmapBitSize() const {
+			return CHAR_BIT * (24*_final_hash.size() + 8*_final_hash.bucket_count());
 		}
-
 		uint64_t totalBitSize() const
 		{
-			// unordered map takes approx 42B per elem [personal test] (42B with uint64_t key, would be larger for other type of elem)
-			return bitset.bitSize() + CHAR_BIT * (_final_hash.size()*42 + sizeof(mphf));
+			return bitvectorBitSize() + hashmapBitSize() + CHAR_BIT * sizeof(mphf);
 		}
 
 	private:
@@ -658,12 +627,12 @@ we need this 2-functors scheme because HashFunctors won't work with unordered_ma
 			for(unsigned ii=0; ii<_nb_levels; ii++)
 			{
 
-				_levels[ii].idx_begin = previous_idx;
+				//_levels[ii].idx_begin = previous_idx;
 
 				// round size to nearest superior multiple of 64, makes it easier to clear a level
-				_levels[ii].hash_domain =  (( (uint64_t) (hash_domain * pow(proba_collision,ii)) + 63) / 64 ) * 64;
-				if(_levels[ii].hash_domain == 0 ) _levels[ii].hash_domain  = 64 ;
-				previous_idx += _levels[ii].hash_domain;
+				_hash_domains[ii] =  (( (uint64_t) (hash_domain * pow(proba_collision,ii)) + 63) / 64 ) * 64;
+				if(_hash_domains[ii] == 0 ) _hash_domains[ii]  = 64 ;
+				previous_idx += _hash_domains[ii];
 
 // 				printf("build level %i bit array : start %12llu, size %12llu  ",ii,_levels[ii].idx_begin,_levels[ii].hash_domain );
 // 				printf(" expected elems : %.2f %% total \n",100.0*pow(proba_collision,ii));
@@ -672,24 +641,60 @@ we need this 2-functors scheme because HashFunctors won't work with unordered_ma
 			return previous_idx;
 		}
 
-		//compute level and returns bit vector position in the last level reached
-		uint64_t hot_fun getLevel(elem_t val, unsigned& level, hash_pair_t& bbhash, unsigned maxlevel = _nb_levels) const
-		{
-			uint64_t bit_idx = ULLONG_MAX;
-			assume(maxlevel <= _nb_levels, "getLevel: maxlevel=%u > _nb_levels=%u", maxlevel, _nb_levels);
-			for (level = 0; level < maxlevel ; level++ )
-			{
-				bit_idx = _levels[level].hash2idx(get_hasher().iter(val, bbhash, level));
-				if(bitset.get(bit_idx))
-					break;
-				else
-					continue;
+
+		template<typename Map> // either mphf or const mphf
+		struct level_finder_tmp {
+			hot_fun forceinline_fun level_finder_tmp(Map& map, const elem_t& key, uint64_t max_level=_nb_levels) : _map(map) {
+				assume(max_level <= _nb_levels, "max_level=%u > _nb_levels=%u", max_level <= _nb_levels);
+				hash_pair_t bbhash;
+				uint64_t bit_begin = 0; // First bit of the current level in the bitvector
+				for(unsigned level = 0; level < _nb_levels; level++) {
+					uint64_t hash = _map.get_hasher().iter(key, bbhash, level);
+					uint64_t hash_domain = _map._hash_domains[level];
+					_level_bit = fastmod64(hash, hash_domain);
+					_bit = _level_bit + bit_begin;
+
+
+					if(level >= max_level) {
+						break;
+					} else if( _map.bitset[_bit]) {
+						_found = true;
+						break;
+					}
+
+					bit_begin += hash_domain;
+				}
 			}
-			return bit_idx;
-		}
+
+			operator bool() { return _found; }
+
+			bool set(bitVector& collisions) const {
+				if(_map.bitset.test_and_set(_bit)) {
+					collisions.test_and_set(_level_bit);
+					return true;
+				} else {
+					return false;
+				}
+			}
+
+			uint64_t rank() const { return _map.bitset.rank(_bit); }
+
+
+		private:
+			Map& _map;
+			uint64_t _bit, _level_bit;
+			bool _found = false;
+
+			uint64_t fastmod64(uint64_t word, uint64_t p) {
+				return (uint64_t)(((__uint128_t)word * (__uint128_t)p) >> 64);
+			}
+		};
+
+		using level_finder = level_finder_tmp<mphf>;
+		using const_level_finder = level_finder_tmp<const mphf>;
 
 		template <typename Range>
-		bool flatten_fun processLevel(const Range& range, unsigned level, buildState& state) {
+		flatten_fun bool processLevel(const Range& range, unsigned level, buildState& state) {
 			switch(level) {
 				case 0: return processLevel_(range, 0, state);
 				case 1: return processLevel_(range, 1, state);
@@ -700,11 +705,13 @@ we need this 2-functors scheme because HashFunctors won't work with unordered_ma
 		template <typename Range>
 		bool processLevel_(const Range& range, unsigned level, buildState& state)
 		{
-			const levelIndexer& level_indexer = _levels[level];
-			if(level > 0)
-				state.collisions_map.clear(0, level_indexer.hash_domain);
+			assume(level < _nb_levels, "level=%u >= _nb_levels", level, _nb_levels);
 
-			bool did_collide = false;
+			uint64_t hash_domain = _hash_domains[level];
+			if(level > 0)
+				state.collisions_map.clear(0, hash_domain);
+
+			bool did_collide = false; // True if we have at least one collision
 			size_t key_idx = 0;
 			for(auto it = std::begin(range) ; it != std::end(range) ; it++, key_idx++) {
 				// After level 2 we can skip keys that were found at stable positions in level 1
@@ -712,36 +719,26 @@ we need this 2-functors scheme because HashFunctors won't work with unordered_ma
 					// This branch is not useless, it allows to inform the branch predictor
 					continue;
 				} else {
-					const elem_t& val = *it;
-					hash_pair_t bbhash; unsigned found_level;
-					getLevel(val, found_level, bbhash, level);
-					if(found_level < level) {
-						// Mark the key as accommodated in previous level, avoid rehasing it afterwards
+					level_finder finder(*this, *it, level);
+					if(finder) {
+						// Mark the key as accommodated in previous level, avoiding rehasing it afterwards
 						state.accommodated.set(key_idx);
 					} else {
-						// Not found in previous levels => insert into this one
-						uint64_t level_hash = get_hasher().iter(val, bbhash, level);
-
-						// Put the key in the bit vector, checking for collisions
-						if(bitset.test_and_set(level_indexer.hash2idx(level_hash)))
-						{
-							state.collisions_map.test_and_set(level_indexer.hash2levelidx(level_hash));
-							did_collide = true;
-						}
+						did_collide |= finder.set(state.collisions_map);
 					}
 				}
 			}
 
 			if(did_collide)
-				bitset.clearCollisions(level_indexer.idx_begin, level_indexer.hash_domain , state.collisions_map);
+				bitset.clearCollisions(state.level_offset, hash_domain , state.collisions_map);
 
+			state.level_offset += hash_domain;
 			return did_collide;
 		}
 
 		template <typename Range>
-		inline void processFallbackLevel(const Range& range, buildState& state)
+		cold_fun void processFallbackLevel(const Range& range, buildState& state, uint64_t next_rank)
 		{
-			uint64_t hashidx = 0;
 			size_t key_idx = 0;
 			for(auto it = std::begin(range) ; it != std::end(range) ; it++, key_idx++) {
 				if(state.accommodated.get(key_idx)) {
@@ -749,11 +746,10 @@ we need this 2-functors scheme because HashFunctors won't work with unordered_ma
 					continue;
 				} else {
 					const elem_t& val = *it;
-					hash_pair_t bbhash;  unsigned found_level;
-					getLevel(val, found_level, bbhash);
-					if(found_level >= _nb_levels) // Not in any level
+					level_finder finder(*this, val);
+					if(!finder) // Not in any level
 					{
-						_final_hash[val] = hashidx++;
+						_final_hash[val] = next_rank++;
 					}
 				}
 			}
@@ -763,7 +759,6 @@ we need this 2-functors scheme because HashFunctors won't work with unordered_ma
 		void save(std::ostream& os) const
 		{
 			os.write(reinterpret_cast<char const*>(&_gamma), sizeof(_gamma));
-			os.write(reinterpret_cast<char const*>(&_lastbitsetrank), sizeof(_lastbitsetrank));
 			os.write(reinterpret_cast<char const*>(&_nelem), sizeof(_nelem));
 			bitset.save(os);
 
@@ -783,7 +778,6 @@ we need this 2-functors scheme because HashFunctors won't work with unordered_ma
 		void load(std::istream& is)
 		{
 			is.read(reinterpret_cast<char*>(&_gamma), sizeof(_gamma));
-			is.read(reinterpret_cast<char*>(&_lastbitsetrank), sizeof(_lastbitsetrank));
 			is.read(reinterpret_cast<char*>(&_nelem), sizeof(_nelem));
 			bitset.load(is);
 
@@ -808,15 +802,15 @@ we need this 2-functors scheme because HashFunctors won't work with unordered_ma
 			}
 		}
 
+		const MultiHasher_t& get_hasher() const { return *this; }
+
 	private:
-		levelIndexer _levels[_nb_levels];
+		uint64_t _hash_domains[_nb_levels];
 		std::unordered_map<elem_t,uint64_t,Hasher_t> _final_hash;
 		bitVector bitset;
 
 		double _gamma;
 		uint64_t _nelem;
-		uint64_t _lastbitsetrank;
-
-		MultiHasher_t get_hasher() const { return {}; }
 	};
 }
+
