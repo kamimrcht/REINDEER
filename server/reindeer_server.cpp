80ea49c9b812e42ab75c32deb0a316319b5f3979
Anthony Boureux <Anthony.Boureux@umontpellier.fr>
2023-08-20 15:45:58 +0200

    [Chg] Rename socket to reindeer_server

--- server/reindeer_server.cpp
+++ server/reindeer_server.cpp
@@ -1,3 +1,33 @@
+/******************************************************************************\
+*                                                                              *
+*  Copyright (C) 2023-      Anthony Boureux                                    *
+*  Copyright (C) 2023       Nathan Boureux                                     *
+*                                                                              *
+*                                                                              *
+*  This file is part of Reindeer program.                                      *
+*                                                                              *
+*  The purpose of Reindeer_server is to run Reindeer:                          *
+*    - load an index                                                           *
+*    - remains in memory until it receive a stop order                         *
+*    - answer to query via a socket                                            *
+*                                                                              *
+*   Reinder_server is free software: you can redistribute it and/or modify     *
+*   it under the terms of the GNU General Public License as published by       *
+*   the Free Software Foundation, either version 3 of the License, or          *
+*   (at your option) any later version.                                        *
+*                                                                              *
+*   Reinder_server is distributed in the hope that it will be useful,          *
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of             *
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the              *
+*   GNU General Public License for more details.                               *
+*                                                                              *
+*   You should have received a copy of the GNU General Public License          *
+*   along with Reindeer program. If not, see <http://www.gnu.org/licenses/>.  *
+*                                                                              *
+*                                                                              *
+*******************************************************************************/
+
+#include <filesystem>
 #include <sys/socket.h> // For socket functions
 #include <netinet/in.h> // For sockaddr_in
 #include <getopt.h>
